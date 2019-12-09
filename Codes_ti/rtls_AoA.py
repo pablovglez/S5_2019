3a0e2e520822af521079e62e1674d0183533e045
Vinicio Gonzalez <vinicio.gonzalez@imt-atlantique.net>
2019-12-09 15:47:41 +0100

    Python script to stock into a file

---
+++ Codes_ti/rtls_AoA.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Nov 16 17:47:15 2019
+
+@author: efisio
+"""
+
+import queue
+import json
+import time
+from rtls import RTLSManager, RTLSNode
+
+master_node = None
+passive_nodes= []
+address = None
+address_type = None
+timestamp = 0
+
+#First, construct an instance of the RTLSNode class for the master and passive
+#device connected to PC. The first parameter is the user/UART COM port, and 
+#the second is the baudrate which defaults to 115200
+
+my_nodes = [RTLSNode('/dev/ttyACM2', 115200), RTLSNode('/dev/ttyACM0', 115200)]
+
+#Instantiate the RTLSManager will the newly created nodes, 
+#but do not connect a rtls_agent_cli.
+
+try:
+    manager = RTLSManager(my_nodes, websocket_port=None)
+
+    #Create a subscriber instance, and add it to the manager. Essentially this 
+    #tells the manager to add messages it receives to the example programs queue 
+    #to be processed. Tell the manager to automatically distribute connection 
+    #parameters to all of the passive nodes. Then start the manager.
+    
+    subscriber = manager.create_subscriber()
+    # Tell the manager to automatically distribute connection parameters
+    manager.auto_params = True
+    # Start RTLS Node threads, Serial threads, and manager thread
+    manager.start()
+    
+    #Poll the capabilities of each node, assign master and create list of passives.
+    
+    # Wait until nodes have responded to automatic identify command and get reference
+    # to single master RTLSNode and list of passive RTLSNode instances
+    master_node, passive_nodes, failed = manager.wait_identified()
+    
+    
+    #Now that we have instantiated the classes, and got the capabilities of the
+    # nodes, we are ready to create the main loop.
+    
+    while True:
+        # Get messages from manager
+        try:
+            identifier, msg_pri, msg = subscriber.pend(block=True, timeout=0.05).as_tuple() #PVGR MSG COMMES FROM SUBSCRIBER?
+    
+            # Pend on activity from one of the nodes with a given timeout.
+    
+            # We received data from one of the nodes
+            # First determine which node sent the information.
+            from_node = master_node.identifier
+            # Get reference to RTLSNode based on identifier in message
+            sending_node = manager[identifier]
+            # Print the message as JSON, map MASTER and PASSIVE based on identifier (BD_ADDR)
+            #if sending_node in passive_nodes:
+                #pass
+                #print(f"PASSIVE: {identifier} --> {msg.as_json()}")
+            #else:
+                #print(f"MASTER: {identifier} --> {msg.as_json()}")
+    
+            # Perform further processing here.
+            if msg.command == 'RTLS_CMD_IDENTIFY':
+                master_node.rtls.scan()
+            if msg.command == 'RTLS_CMD_SCAN' and msg.type == 'AsyncReq':
+                address = msg.payload.addr
+                address_type = msg.payload.addrType
+            timestamp = time.time()
+            try:
+                with open("/home/efisio/ti/simplelink_cc2640r2_sdk_3_20_00_21/tools/blestack/rtls_agent/examples/data.json","a") as fichier:
+                    message=json.loads("["+msg.as_json()+"]")
+                    message[0]["payload"]["time"]=timestamp
+                    #message[0]["payload"]["address"]=address #AT THIS TIME IS NOT KNOWN NOR NECESSARY
+                    json.dump(message[0]["payload"], fichier)
+                    print(message[0]["payload"])
+                    fichier.write('\n')
+                    fichier.close
+            except:
+                break
+                    
+        except queue.Empty:
+            pass
+        except (KeyboardInterrupt, SystemExit):
+            print ('\nkeyboardinterrupt caught (again)')
+            print ('\n...Program Stopped Manually!')
+            break
+
+except:
+    raise
--- Docs/Notas/Install-SimpleLink-CC2640R2-SDK.md
+++ Docs/Notas/Install-SimpleLink-CC2640R2-SDK.md
@@ -4,43 +4,58 @@
 
 # Go to download folder in terminal and type:
 
-#Give permissions to file typing in terminal chmod +x simplelink_cc2640r2_sdk_3_20_00_21.run
+#Give permissions to file typing in terminal 
+chmod +x simplelink_cc2640r2_sdk_3_20_00_21.run
 
-#Type in terminal -/simplelink_cc2640r2_sdk_3_20_00_21.run
+#Type in terminal 
+-/simplelink_cc2640r2_sdk_3_20_00_21.run
 
 #Look for the rtls_monitor tool located in [ti --> simplelink_cc2640r2_sdk_3_20_00_21 --> tools --> blestack --> rtls_agent]
 
-#Give permissions to file typing in terminal chmod +x requirements.txt
+#Give permissions to file typing in terminal 
+chmod +x requirements.txt
 
-#Install virtualenv typing sudo pip3 install virtualenv 
+#Install virtualenv typing 
+sudo pip3 install virtualenv 
 
-#virtualenv -p python3 .venv or python3 -m venv .venv
+virtualenv -p python3 .venv 
+#or python3 -m venv .venv
 
-#. .venv/bin/activate # or source .venv/bin/activate which does exactly the same thing
+. .venv/bin/activate 
+# or source .venv/bin/activate which does exactly the same thing
 
-#Give permissions to file typing in terminal chmod +x rtls_agent_cli
+#Give permissions to file typing in terminal 
+chmod +x rtls_agent_cli
 
-#Make sure all pre-requisites are fulfilled by typing: pip3 install -r requirements.txt 
+#Make sure all pre-requisites are fulfilled by typing: 
+pip3 install -r requirements.txt 
 
 #If error with windows-curses [windows-curses is not necessary on Python 3.7] comment such line
 
 #### NOW YOU CAN RUN THE RTLS AGENT
 
-#python3 agent/rtls_agent_cli.py
+python3 agent/rtls_agent_cli.py
 
 ########## TO RUN AGAIN THE VIRTUAL ENVIRONMENT ####
 
 
 ### FOR THE FOLLOWING USES, START FROM  LINE 17 ###
 
-#virtualenv -p python3 .venv or python3 -m venv .venv
+virtualenv -p python3 .venv 
+#or python3 -m venv .venv
 
-#. .venv/bin/activate # or source .venv/bin/activate which does exactly the same thing
+. .venv/bin/activate 
+# or source .venv/bin/activate which does exactly the same thing
 
-#cd /home/efisio/ti/simplelink_cc2640r2_sdk_3_20_00_21/tools/blestack/rtls_agent
+cd /home/efisio/ti/simplelink_cc2640r2_sdk_3_20_00_21/tools/blestack/rtls_agent
 
-#chmod +x rtls_agent_cli
+chmod +x rtls_agent_cli
 
-#pip3 install -r requirements.txt
+pip3 install -r requirements.txt
 
-#python3 agent/rtls_agent_cli.py
+python3 agent/rtls_agent_cli.py
+
+### TO RUN THE AOA CLIENT ###
+
+cd /home/efisio/ti/simplelink_cc2640r2_sdk_3_20_00_21/tools/blestack/rtls_agent/examples
+
